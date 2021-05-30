385832d579b393efa3a0013f7c108e1ebe248619
Bhavuk-Garg <38847938+Bhavuk-Garg@users.noreply.github.com>
2021-05-30 22:42:05 +0530

    added spinner

--- app/src/main/java/com/example/codelab/OrderActivity.java
+++ app/src/main/java/com/example/codelab/OrderActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,16 @@ public class OrderActivity extends AppCompatActivity {
         String orderMessage=intent.getStringExtra(MainActivity.orderTextExtra);
         TextView textView=(TextView)findViewById(R.id.ordertext);
         textView.setText(orderMessage);
+
+        Spinner spinner=(Spinner)findViewById(R.id.label_spinner);
+        if(spinner!=null){
+            spinner.setOnItemSelectedListener(this);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.labels_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource
+                    (android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
     }
     private void displayToast(String message){
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
@@ -26,5 +39,16 @@ public class OrderActivity extends AppCompatActivity {
         RadioButton delivery=(RadioButton)view;
         String deliveryMethod=delivery.getText().toString();
         displayToast(deliveryMethod);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String location=parent.getItemAtPosition(position).toString();
+            displayToast(location);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
