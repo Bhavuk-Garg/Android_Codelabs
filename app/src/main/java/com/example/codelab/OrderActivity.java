8fcee7bf74ba74343830775f25902de0941357fa
Bhavuk-Garg <38847938+Bhavuk-Garg@users.noreply.github.com>
2021-05-30 22:21:57 +0530

    added input fields

--- app/src/main/java/com/example/codelab/OrderActivity.java
+++ app/src/main/java/com/example/codelab/OrderActivity.java
@@ -3,7 +3,10 @@ package com.example.codelab;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -15,5 +18,13 @@ public class OrderActivity extends AppCompatActivity {
         String orderMessage=intent.getStringExtra(MainActivity.orderTextExtra);
         TextView textView=(TextView)findViewById(R.id.ordertext);
         textView.setText(orderMessage);
+    }
+    private void displayToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+    public void ondeliveryMethod(View view){
+        RadioButton delivery=(RadioButton)view;
+        String deliveryMethod=delivery.getText().toString();
+        displayToast(deliveryMethod);
     }
 }
